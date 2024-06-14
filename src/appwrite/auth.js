eef306c91bf1148f33dabe46252d33c2e8d58617
asadullah abbasi <asadullahabbasi225@gmail.com>
2024-06-14 22:17:33 +0500

    asyn func

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,6 +1,6 @@
 import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
-export class AuthService {
+export class AuthService { //made a class which has a client property and inside the constructor we set the endpoint and project id of the appwrite account
     Client = new Client();
     constructor() {
         console.log(Client);
